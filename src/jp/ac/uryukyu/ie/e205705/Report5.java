66583dde5609ea681e7f79f5c3c6b9e9376da5b2
Kanta TAMAKI <e205705@ie.u-ryukyu.ac.jp>
2020-12-20 15:03:09 +0900

    step1

---
+++ src/jp/ac/uryukyu/ie/e205705/Report5.java
@@ -0,0 +1,9 @@
+package jp.ac.uryukyu.ie.e205705;
+
+public class Report5 {
+    public static void main(String[] args){
+        String str = null;
+        System.out.println(str.length());
+    }
+    
+}
